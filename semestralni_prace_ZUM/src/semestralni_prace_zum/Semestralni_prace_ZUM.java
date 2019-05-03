a797a8f010fdd2a294fbed91354e6e72cc714861
Alexandra Parkhomenko <parkhal1@fit.cvut.cz>
2019-05-03 17:32:09 +0200

    1st

---
+++ semestralni_prace_ZUM/src/semestralni_prace_zum/Semestralni_prace_ZUM.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semestralni_prace_zum;
+
+/**
+ *
+ * @author parkhal1
+ */
+public class Semestralni_prace_ZUM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
